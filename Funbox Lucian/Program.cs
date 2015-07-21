f725f9ea832943ed13f71aca59a156d87df08355
funboxxx <funboxxx@wp.pl>
2015-07-21 16:44:57 +0100

    harras Q if laneclear, lasthit, mixed

--- Funbox Lucian/Program.cs
+++ Funbox Lucian/Program.cs
@@ -48,6 +48,13 @@ private static void Orbwalking_AfterAttack(AttackableUnit unit, AttackableUnit t
   var targett = TargetSelector.GetTarget(900, TargetSelector.DamageType.Physical);
   if (unit.IsMe)
     {
+      if (_orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear || _orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LastHit || _orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed)
+        {
+          if (_q.IsReady())
+            {
+              CastQ();
+            }
+        }
       if (_orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
         {
           if (ec)
